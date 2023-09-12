024dc1dad4d52b5b5d2121cd27ca863a07502b62
tapfer-abdullah <tapfer.abdullah@gmail.com>
2023-09-12 11:13:27 +0600

    updating code

--- index.js
+++ index.js
@@ -166,7 +166,7 @@ async function run() {
       res.send(result);
     });
 
-    app.patch("/medicine-status/:id", async(req, res) => {
+    app.patch("/medicine-status/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const updateStatus = {

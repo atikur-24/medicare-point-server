80d520db05bf6ee9935ace62afbe6b0d00daae1e
unknown <sihabuddinmolla11@gmail.com>
2023-09-22 08:18:15 +0600

    fixed api fro order history detail

--- index.js
+++ index.js
@@ -370,7 +370,7 @@ async function run() {
     app.get("/medicinesOrderByAdmin/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
-      const result = await orderedMedicinesCollection.find(query).toArray();
+      const result = await orderedMedicinesCollection.findOne(query);
       res.send(result);
     });
 
